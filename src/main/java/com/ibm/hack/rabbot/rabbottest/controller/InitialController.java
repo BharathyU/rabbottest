22954f02ea87006638b8871e75e89337d33b64cd
BUD3566 <bharathykannan.udayasuriyan@external.toyota-europe.com>
2020-08-11 13:10:00 +0530

    Personality Insights

--- src/main/java/com/ibm/hack/rabbot/rabbottest/controller/InitialController.java
+++ src/main/java/com/ibm/hack/rabbot/rabbottest/controller/InitialController.java
@@ -1,18 +1,15 @@
 package com.ibm.hack.rabbot.rabbottest.controller;
 
 
-import com.google.gson.stream.JsonReader;
 import com.ibm.cloud.sdk.core.security.IamAuthenticator;
-import com.ibm.cloud.sdk.core.util.GsonSingleton;
 import com.ibm.watson.personality_insights.v3.PersonalityInsights;
-import com.ibm.watson.personality_insights.v3.model.Content;
 import com.ibm.watson.personality_insights.v3.model.Profile;
 import com.ibm.watson.personality_insights.v3.model.ProfileOptions;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 
 @RestController
 public class InitialController {
@@ -23,8 +20,8 @@ public class InitialController {
     }
 
 
-    @GetMapping(path = "/personality")
-    public Profile personalityTest(){
+    @PostMapping(path = "/personality")
+    public Profile personalityTest(@RequestBody String text){
 
 
         IamAuthenticator authenticator = new IamAuthenticator("O4bix_w7_6v2sBgFm5MSNgkbrKLzQVpK3A1NSq2-spR4");
@@ -32,24 +29,20 @@ public class InitialController {
         personalityInsights.setServiceUrl("https://api.eu-gb.personality-insights.watson.cloud.ibm.com/instances/6f887956-f3a3-48f2-ac89-6f6b38f28065");
 
         try {
-            ClassLoader classLoader = getClass().getClassLoader();
-            JsonReader jsonReader =
-                    new JsonReader(new FileReader(classLoader.getResource("profile.json").getFile()));
-            Content content =
-                    GsonSingleton.getGson().fromJson(jsonReader, Content.class);
+
+
 
             ProfileOptions profileOptions = new ProfileOptions.Builder()
-                    .content(content)
+                    .text(text)
                     .consumptionPreferences(true)
                     .rawScores(true)
                     .build();
 
             Profile profile =
                     personalityInsights.profile(profileOptions).execute().getResult();
-            System.out.println(profile);
 
             return profile;
-        } catch (FileNotFoundException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
